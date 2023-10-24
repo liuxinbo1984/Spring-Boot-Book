5bad162f0337b1584ea31988a7764a780336e09d
liuxinbo.1984 <liuxinbo.1984@163.com>
2023-10-24 14:28:31 +0800

    update springboot parent

--- 04/HelloWorldBoot/src/main/java/com/example/demo/HelloWorldApplication.java
+++ 04/HelloWorldBoot/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import javafx.application.Application;
-import javafx.scene.Parent;
+//import javafx.application.Application;
+//import javafx.scene.Parent;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
